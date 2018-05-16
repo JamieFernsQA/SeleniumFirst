1f735eda925b3a822b313133aa37b84d0ebac33e
JamieFernsQA <Jamie.Ferns@AcademyTrainee.com>
2018-05-16 10:08:49 +0100

    initial commit

--- src/test/java/SeleniumFirst/ChromeDriverTest.java
+++ src/test/java/SeleniumFirst/ChromeDriverTest.java
@@ -2,6 +2,7 @@ package SeleniumFirst;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,6 +13,15 @@ public class ChromeDriverTest {
     @Before
     public void setup() {
         driver = new ChromeDriver();
+    }
+
+    @Test
+
+    public void exampleSelenium() throws InterruptedException {
+        driver.manage().window().fullscreen();
+        Thread.sleep(1000);
+        driver.get("http://www.google.com/");
+        Thread.sleep(1000);
     }
     @After
     public void tearDown(){
